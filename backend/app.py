0392f8524329f2ac582c3ef02d1ad178ad210ef5
rudresh-coder <rudreshnaidu.66@gmail.com>
2026-02-13 22:10:37 +0530

    front-end folder

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 import mysql.connector
 
 app = Flask(__name__)
-
+CORS(app)
 db = mysql.connector.connect(
     host=os.getenv("DB_HOST", "localhost"),
     user=os.getenv("DB_USER", "placement_user"),
